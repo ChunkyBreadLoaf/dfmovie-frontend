c008c8d7056d3d48bd4c185d9a0391dc82acf180
ChunkyBreadLoaf <ntlong.bm01@gmail.com>
2022-03-06 08:57:35 +0700

    Added foundation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { MovieInfoComponent } from './pages/movie-info/movie-info.component';
+import { Error404Component } from './pages/error404/error404.component';
+import { Error403Component } from './pages/error403/error403.component';
+import { SearchComponent } from './pages/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingPageComponent,
+    HomepageComponent,
+    LoginComponent,
+    SignupComponent,
+    MovieInfoComponent,
+    Error404Component,
+    Error403Component,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
